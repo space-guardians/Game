1167977048aab3b7e9ff3290629957dc392981ec
Panpan76 <cedric.moclyn@hotmail.fr>
2018-03-05 10:50:39 +0100

    Config apache

---
+++ src/Controller/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController
+{
+
+    /**
+     * @route("/login", name="login")
+     */
+    public function login()
+    {
+    }
+}
